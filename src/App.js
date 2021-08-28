ca678163e59fd98b04c2439a70a7a9983b4097ca
junya takaesu <junya.takaesu@gmail.com>
2021-08-28 14:08:44 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StateCounter from "./Components/StateCounter";
+
+function App() {
+  return (
+    <div className="container">
+      <StateCounter />
+    </div>
+  );
+}
+
+export default App;
